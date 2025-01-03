d9df96092b4ce5f7751c7145250f0388284b1158
ezeekiel3 <lolochapa10@hotmail.com>
2025-01-03 16:17:29 -0300

    se agregaron cosas

--- sketch_250102b/sketch_250102b.js
+++ sketch_250102b/sketch_250102b.js
@@ -1,5 +1,6 @@
 let fondoPrincipal
 const botonJugar = new Boton('JUGAR', 105, 405, 110, 90, 22, 68, 408)
+const botonInicio = new Boton('INICIO', 640 / 2, 420, 110, 90, 22, 290, 420)
 let desierto
 let Jesus
 let botonImg
@@ -10,6 +11,7 @@ let vidaContador = 3
 let velJesus = 5
 let posXRandom = []
 let piedrasImg = []
+let piedrasTocadas = []
 let velPiedra = 8
 let posYPiedra = []
 let posXJesus
@@ -25,6 +27,7 @@ function preload() {
         piedrasImg[i] = loadImage('./img/Piedra.png')
     }
     vida = loadImage('./img/vida.png')
+    hit = loadImage('./img/choca.png')
 }
 
 function setup() {
@@ -34,6 +37,7 @@ function setup() {
     for (let i = 0; i < piedrasImg.length; i++) {
         posXRandom[i] = random(-130, 450)
         posYPiedra[i] = startYPiedra + i * offsetYPiedra
+        piedrasTocadas[i] = false
     }
 }
 
@@ -65,17 +69,20 @@ function draw() {
 
             // Dibujar piedra
             image(piedrasImg[i], piedraX + 35, piedraY - 35, 350, 350)
-            fill(0, 255, 73, 50)
-            rect(piedraX, piedraY, piedraAncho / 4, piedraAlto / 4)
-            fill('blue')
-            rect(posXJesus, 330, 45, 140)
+            // fill(0, 255, 73, 50)
+            // rect(piedraX, piedraY, piedraAncho / 4, piedraAlto / 4)
+            // fill('blue')
+            // rect(posXJesus, 330, 45, 140)
 
             if (
+                !piedrasTocadas[i] &&
                 piedraX + piedraAncho / 4 > posXJesus && // Borde derecho de la piedra cruza borde izquierdo del rectángulo
                 piedraX < posXJesus + 45 && // Borde izquierdo de la piedra cruza borde derecho del rectángulo
                 piedraY + piedraAlto / 4 > 330 && // Borde inferior de la piedra cruza borde superior del rectángulo
                 piedraY < 330 + 140 // Borde superior de la piedra cruza borde inferior del rectángulo
             ) {
+                image(hit, 640 / 2, 480 / 2)
+                piedrasTocadas[i] = true
                 vidaContador--
             }
         }
@@ -89,13 +96,23 @@ function draw() {
         } else if (vidaContador === 1) {
             image(vida, 30, 450, 70, 40)
         } else {
-            // PANTALLA DE DERROTA
+            estado = 4
         }
+    } else if (estado === 4) {
+        // PANTALLA DE DERROTA
+        vidaContador = 3
+        background(100)
+        fill('black')
+        textSize(50)
+        text('PERDISTE GATO', 120, 480 / 2)
+        botonInicio.dibujarBoton(botonImg)
     }
 }
 
 function mouseClicked() {
     if (mouseX > 50 && mouseX < 50 + 110 && mouseY > 360 && mouseY < 360 + 90) {
         estado = 1
+    } else if (mouseX > 270 && mouseX < 270 + 105 && mouseY > 375 && mouseY < 375 + 90) {
+        estado = 0
     }
 }
