6b014b5dad26be861a02f53b5a883e8634d595a9
ezeekiel3 <lolochapa10@hotmail.com>
2025-01-04 11:27:58 -0300

    se completo el juego, pero faltan arreglar bugs y agregar mas cosas

--- sketch_250102b/sketch_250102b.js
+++ sketch_250102b/sketch_250102b.js
@@ -11,12 +11,14 @@ let vidaContador = 3
 let velJesus = 5
 let posXRandom = []
 let piedrasImg = []
-let piedrasTocadas = []
+let piedrasTocadas = [] // Array para registrar si una piedra ya tocó a Jesús
+let piedrasProcesadas = 0 // Contador de piedras procesadas
+const totalPiedras = 30 // Total de piedras
 let velPiedra = 8
 let posYPiedra = []
 let posXJesus
 const startYPiedra = -300
-const offsetYPiedra = -120
+const offsetYPiedra = -100
 
 function preload() {
     fondoPrincipal = loadImage('./img/fondoPrincipal.png')
@@ -35,84 +37,121 @@ function setup() {
     rectMode(CORNER)
     imageMode(CENTER)
     for (let i = 0; i < piedrasImg.length; i++) {
-        posXRandom[i] = random(-130, 450)
+        posXRandom[i] = random(0, 580)
         posYPiedra[i] = startYPiedra + i * offsetYPiedra
         piedrasTocadas[i] = false
     }
 }
 
 function draw() {
-    if (estado === 0) {
-        image(fondoPrincipal, 640 / 2, 480 / 2, 640, 480)
-        fill('black')
-        textSize(50)
-        text('La carrera de Jesus', 100, 50)
-        botonJugar.dibujarBoton(botonImg)
-    } else if (estado === 1) {
-        image(desierto, 640 / 2, 480 / 2)
-        if (keyIsDown(LEFT_ARROW) && pasos > -35) {
-            pasos--
-        }
-        if (keyIsDown(RIGHT_ARROW) && pasos < 80) {
-            pasos++
-        }
-        posXJesus = 180 + pasos * velJesus
-        image(Jesus, posXJesus + 20, 400, 250, 200)
-
-        for (let i = 0; i < piedrasImg.length; i++) {
-            let piedraY = posYPiedra[i]++ * velPiedra
-            let piedraX = posXRandom[i]
-
-            // Dimensiones ajustadas de las piedras
-            let piedraAncho = 220 // Cambia según el tamaño visual
-            let piedraAlto = 220 // Cambia según el tamaño visual
-
-            // Dibujar piedra
-            image(piedrasImg[i], piedraX + 35, piedraY - 35, 350, 350)
-            // fill(0, 255, 73, 50)
-            // rect(piedraX, piedraY, piedraAncho / 4, piedraAlto / 4)
-            // fill('blue')
-            // rect(posXJesus, 330, 45, 140)
-
-            if (
-                !piedrasTocadas[i] &&
-                piedraX + piedraAncho / 4 > posXJesus && // Borde derecho de la piedra cruza borde izquierdo del rectángulo
-                piedraX < posXJesus + 45 && // Borde izquierdo de la piedra cruza borde derecho del rectángulo
-                piedraY + piedraAlto / 4 > 330 && // Borde inferior de la piedra cruza borde superior del rectángulo
-                piedraY < 330 + 140 // Borde superior de la piedra cruza borde inferior del rectángulo
-            ) {
-                image(hit, 640 / 2, 480 / 2)
-                piedrasTocadas[i] = true
-                vidaContador--
+    switch (estado) {
+        case 1:
+            image(desierto, 640 / 2, 480 / 2)
+            if (keyIsDown(LEFT_ARROW) && pasos > -35) {
+                pasos--
             }
-        }
-        if (vidaContador === 3) {
-            image(vida, 30, 450, 70, 40)
-            image(vida, 65, 450, 70, 40)
-            image(vida, 100, 450, 70, 40)
-        } else if (vidaContador === 2) {
-            image(vida, 30, 450, 70, 40)
-            image(vida, 65, 450, 70, 40)
-        } else if (vidaContador === 1) {
-            image(vida, 30, 450, 70, 40)
-        } else {
-            estado = 4
-        }
-    } else if (estado === 4) {
-        // PANTALLA DE DERROTA
-        vidaContador = 3
-        background(100)
-        fill('black')
-        textSize(50)
-        text('PERDISTE GATO', 120, 480 / 2)
-        botonInicio.dibujarBoton(botonImg)
+            if (keyIsDown(RIGHT_ARROW) && pasos < 80) {
+                pasos++
+            }
+            posXJesus = 180 + pasos * velJesus
+            image(Jesus, posXJesus + 20, 400, 250, 200)
+
+            for (let i = 0; i < piedrasImg.length; i++) {
+                let piedraY = posYPiedra[i]++ * velPiedra
+                let piedraX = posXRandom[i]
+
+                // Dibujar piedra
+                image(piedrasImg[i], piedraX + 35, piedraY - 35, 350, 350)
+
+                if (
+                    !piedrasTocadas[i] &&
+                    piedraX + 55 > posXJesus && // Borde derecho de la piedra cruza borde izquierdo del rectángulo
+                    piedraX < posXJesus + 45 && // Borde izquierdo de la piedra cruza borde derecho del rectángulo
+                    piedraY + 55 > 330 && // Borde inferior de la piedra cruza borde superior del rectángulo
+                    piedraY < 330 + 140 // Borde superior de la piedra cruza borde inferior del rectángulo
+                ) {
+                    image(hit, 640 / 2, 480 / 2)
+                    piedrasTocadas[i] = true
+                    vidaContador--
+                    piedrasProcesadas++ // Incrementar contador de piedras procesadas
+                }
+
+                if (piedraY > height && !piedrasTocadas[i]) {
+                    piedrasTocadas[i] = true // Marcar como procesada
+                    piedrasProcesadas++ // Incrementar contador al salir de pantalla
+                }
+            }
+
+            fill('green')
+            rect(10, height - (height - 10), (piedrasProcesadas / totalPiedras) * (width - 20), 10)
+            noFill()
+            stroke('white')
+            rect(10, height - (height - 10), width - 20, 10) // Marco de la barra
+
+            if (piedrasProcesadas === totalPiedras) {
+                estado = 2
+                for (let i = 0; i < piedrasImg.length; i++) {
+                    posXRandom[i] = random(0, 580)
+                    posYPiedra[i] = startYPiedra + i * offsetYPiedra
+                    piedrasTocadas[i] = false
+                }
+            }
+
+            switch (vidaContador) {
+                case 3:
+                    image(vida, 30, 450, 70, 40)
+                    image(vida, 65, 450, 70, 40)
+                    image(vida, 100, 450, 70, 40)
+                    break
+                case 2:
+                    image(vida, 30, 450, 70, 40)
+                    image(vida, 65, 450, 70, 40)
+                    break
+                case 1:
+                    image(vida, 30, 450, 70, 40)
+                    break
+                default:
+                    estado = 4
+                    vidaContador = 3
+                    piedrasProcesadas = 0
+                    for (let i = 0; i < piedrasImg.length; i++) {
+                        posXRandom[i] = random(0, 580)
+                        posYPiedra[i] = startYPiedra + i * offsetYPiedra
+                        piedrasTocadas[i] = false
+                    }
+                    break
+            }
+            break
+        case 2:
+            // PANTALLA DE VICTORIA
+            background(100)
+            fill('black')
+            textSize(50)
+            text('GANASTE, MUY BIEN', 60, 480 / 2)
+            botonInicio.dibujarBoton(botonImg)
+            break
+        case 4:
+            // PANTALLA DE DERROTA
+            background(100)
+            fill('black')
+            textSize(50)
+            text('PERDISTE GATO', 120, 480 / 2)
+            botonInicio.dibujarBoton(botonImg)
+            break
+        default:
+            image(fondoPrincipal, 640 / 2, 480 / 2, 640, 480)
+            fill('black')
+            textSize(50)
+            text('La carrera de Jesus', 100, 50)
+            botonJugar.dibujarBoton(botonImg)
+            break
     }
 }
 
 function mouseClicked() {
     if (mouseX > 50 && mouseX < 50 + 110 && mouseY > 360 && mouseY < 360 + 90) {
         estado = 1
-    } else if (mouseX > 270 && mouseX < 270 + 105 && mouseY > 375 && mouseY < 375 + 90) {
+    } else if (mouseX > 270 && mouseX < 270 + 105 && mouseY > 375 && mouseY < 375 + 90 && estado !== 1) {
         estado = 0
     }
 }
